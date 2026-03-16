fb8afc89bdd1043cd555bed7ff36ebe5c608baab
zhaojun <zhaojun.li@gmail.com>
2026-03-16 00:31:39 -0400

    add new table

--- dev/init.sql
+++ dev/init.sql
@@ -12,3 +12,16 @@ CREATE TABLE IF NOT EXISTS `job_request` (
   `user_id` bigint DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `job_execution_report` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `start_time` datetime(6),
+  `end_time` datetime(6),
+  `execution_node` varchar(255) DEFAULT NULL,
+  `execution_log` longtext,
+  `status` varchar(255) NOT NULL,
+  `job_request_id` bigint DEFAULT NULL,
+  `user_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ux_job_execution_report__job_request_id` (`job_request_id`),
+  CONSTRAINT `fk_job_execution_report__job_request_id` FOREIGN KEY (`job_request_id`) REFERENCES `job_request` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
